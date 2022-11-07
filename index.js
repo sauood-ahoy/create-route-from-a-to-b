415cea6bd415e9e8c082ceba4e1508ef22f09ec2
sauood-ahoy <105483648+sauood-ahoy@users.noreply.github.com>
2022-11-07 16:54:02 +0400

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
-// Import stylesheets
-import './style.css';
+const mapElement = document.querySelector('#mapContainer');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let platform;
+let defaultLayers;
+let map;
+
+async function initializeMap() {
+  platform = await AhoyMapView.service().Platform(SUBSCRIPTION_KEY);
+  defaultLayers = platform.createDefaultLayers();
+  map = AhoyMapView.Map(mapElement, defaultLayers.vector.normal.map, {
+    zoom: 4,
+    center: {
+      lat: 34,
+      lng: 41,
+    },
+  });
+
+  // default interactions for pan/zoom
+  const mapEvents = AhoyMapView.mapevents().MapEvents(map);
+  const behavior = AhoyMapView.mapevents().Behavior(mapEvents);
+
+  // Create the default UI components
+  const ui = AhoyMapView.ui().UI.createDefault(map, defaultLayers);
+  addMarkerAtPoints();
+}
+async function createRoute() {
+  const createrRouteRes = await AhoyMapView.createRoute({
+    transportMode: 'car',
+    origin: { lat: 25.118709, lng: 55.200736 },
+    destination: { lat: 25.237545, lng: 55.301214 },
+  });
+  createrRouteRes.forEach((route) => {
+    let linestring = AhoyMapView.getCoOrdinates().fromFlexiPolyline(
+      route.polyline
+    );
+    // Create a polyline to display the route:
+    let routeLine = AhoyMapView.polyline(4, 'blue', linestring);
+    // Add the route polyline and the two markers to the map:
+    map.addObjects([routeLine]);
+    // Set the map's viewport to make the whole route visible:
+    map
+      .getViewModel()
+      .setLookAtData({ bounds: routeLine.getBoundingBox(), zoom: 12 });
+  });
+}
+
+function addMarkerAtPoints() {
+  const markerA = AhoyMapView.map().createMarker({
+    lat: 25.118709,
+    lng: 55.200736,
+  });
+  const markerB = AhoyMapView.map().createMarker({
+    lat: 25.237545,
+    lng: 55.301214,
+  });
+  map.addObject(markerA);
+  map.addObject(markerB);
+}
+
+initializeMap();
+createRoute();
